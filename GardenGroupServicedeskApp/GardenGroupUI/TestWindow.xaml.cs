7253750f5746db39b9f1b5cd586f1b84f9666fbb
Hashcat <ZeHashcat@gmail.com>
2022-09-30 20:14:11 +0200

    Example finished.
    Kinda.

--- GardenGroupServicedeskApp/GardenGroupUI/TestWindow.xaml.cs
+++ GardenGroupServicedeskApp/GardenGroupUI/TestWindow.xaml.cs
@@ -23,40 +23,61 @@ namespace GardenGroupUI
     {
         //NOTE: All this code prolly belongs in DAO I think, this is just an example on an example branch.
         private MongoClient client;
+        //NOTE: ↓↓↓↓↓-I think this establishes a connection with the database.
         private IMongoDatabase database = null;
+        //NOTE: ↓↓↓↓↓-I think this establishes a connection with a collection within the database.
         private IMongoCollection<BsonDocument> collection = null;
-
 
         //EXPLANATION: This is just an example to show how to create a BsonDocument, you can leave away the new Bson<class>() on each line and input the variable directy.
         //NOTE: could potentially lead to problems where multiple types of Bson<class> are possible and it picks the wrong one.
         //NOTE: For example, { "_id", 1 } should work, since it accepts variables as a BsonValue which should automatically convert it to an BsonInt32.
         private BsonDocument user = new BsonDocument
             {
-                { "_id", new BsonInt32(1) },
-                { "userId", new BsonInt32(12345) },
-                { "firstName", new BsonString("Mylo") },
-                { "lastName", new BsonString("Bronkhorst") },
-                { "password", new BsonString("thisIsTotallyHashedRn") },
-                { "acountActive", new BsonBoolean(false) },
-                { "privileges", new BsonDocument
+                { "_id", new BsonInt32(1) }, //EXPLANATION: <--- Leave this out and it will assign an _id automatically, not sure what pro's and cons are for auto and manual.
+                { "User Id", new BsonInt32(12345) }, //EXPLANATION: <----Actual employeenumber
+                { "First Name", new BsonString("Mylo") },
+                { "Last Name", new BsonString("Bronkhorst") },
+                { "Password", new BsonString("thisIsTotallyHashedRn") }, //NOTE: <----Should already be hashed and salted at this point.
+                { "Acount_isConnected", new BsonBoolean(false) }, //NOTE: Not gonna need this, just to illustrate the bool.
+                { "Role", new BsonString("Sudo") //NOTE*: A clear system for privileges is needed, See "NOTE*:" Below.
+                },
+                { "Teams", new BsonDocument
                     {
-                        { "privilege1", new BsonString("User") },
-                        { "privilege2", new BsonString("Employee") },
-                        { "privilege3", new BsonString("Admin") },
-                        { "privilege4", new BsonString("Sudo") },
+                        { "Team1", new BsonInt32(3) },
+                        { "Team2", new BsonInt32(7) },
+                        //NOTE: etc.
+                    }
+                }
+            };
+
+        //NOTE*: I had the idea to hardcode the "Sudo" role into this app and have roles such as admin, employee and user defined as docs in the database.
+        //NOTE*: There should always be at least one Sudo role user in the entire database. Only a Sudo role user may CRUD itself and other Sudo's.
+        //NOTE*: This way, new roles could potentially be defined and assigned within the app.
+        //NOTE*: So when this app checks for privileges it would query the following document for example.
+        private BsonDocument employee = new BsonDocument
+            {
+                { "_id", new BsonString("Employee") }, //EXPLANATION: <--- Role as _id so it is unique.
+                { "User", new BsonDocument //EXPLANATION: <---Refers to a Collection named User.
+                    {   //EXPL./QUE.:↓↓↓↓↓-CRUD rights, anything else needed?
+                        { "Create", new BsonBoolean(false) }, //QUESTION: <---- Should this be false?
+                        { "Read", new BsonBoolean(true) },
+                        { "Update", new BsonBoolean(true) },
+                        { "Delete", new BsonBoolean(false) },
                     }
                 },
-                { "teams", new BsonDocument
-                    {
-                        { "team1", new BsonInt32(3) },
-                        { "team2", new BsonInt32(7) },
+                { "Ticket", new BsonDocument //EXPLANATION: <---Refers to a Collection named Ticket.
+                    {   //EXPL./QUE.:↓↓↓↓↓-CRUD rights, anything else needed?
+                        { "Create", new BsonBoolean(true) },
+                        { "Read", new BsonBoolean(true) },
+                        { "Update", new BsonBoolean(true) },
+                        { "Delete", new BsonBoolean(true) },
                     }
                 }
             };
 
         public TestWindow(MongoClient client)
         {
-            this.client = client;
+            this.client = client; // Client is given from MainWindow
             InitializeComponent();
         }
 
@@ -64,21 +85,21 @@ namespace GardenGroupUI
         {
             string databaseName = textBoxSetDatabase.Text;
             labelCurrentDatabase.Content = databaseName;
-            database = client.GetDatabase(databaseName);           
+            database = client.GetDatabase(databaseName);        //NOTE: Doing this establishes a connection with the database I think.   
         }
 
         private void buttonSetCollection_Click(object sender, RoutedEventArgs e)
         {
             string collectionName = textBoxSetCollection.Text;
             labelCurrentCollection.Content = collectionName;
-            collection = database.GetCollection<BsonDocument>(collectionName);
+            collection = database.GetCollection<BsonDocument>(collectionName); //NOTE: Doing this establishes a connection with a collection within the database I think.
         }
 
         private void buttonCreateUser_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                collection.InsertOne(user);
+                collection.InsertOne(user); //EXPLANATION: This actually inserts a document into the database.
                 MessageBox.Show("Document inserted!");
             }
             catch (Exception ex)
@@ -91,8 +112,35 @@ namespace GardenGroupUI
         {
             try
             {
-                FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("counter", "Mylo");
-                BsonDocument document = collection.Find(filter).First();
+                FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("firstName", "Mylo"); //EXPLANATION: Filters documents on key firstname with value "Mylo" only, a collection must be set at this point.
+                BsonDocument document = collection.Find(filter).First(); //EXPLANATION: Finds and returns first document with filter applied.
+                textBlockDocumentOutput.Text = document.ToString();
+            }
+            catch (Exception ex)
+            {
+                textBlockDocumentOutput.Text = ex.Message;
+            }
+        }
+
+        private void buttonCreateRole_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                collection.InsertOne(user); //EXPLANATION: This actually inserts a document into the database.
+                MessageBox.Show("Document inserted!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonFindRole_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", "12345"); //EXPLANATION: Filters documents on key "_id" with value "12345" only, a collection must be set at this point.
+                BsonDocument document = collection.Find(filter).First(); //EXPLANATION: Finds and returns first document with filter applied. since it filters on an unique id this will only ever return one document.
                 textBlockDocumentOutput.Text = document.ToString();
             }
             catch (Exception ex)
