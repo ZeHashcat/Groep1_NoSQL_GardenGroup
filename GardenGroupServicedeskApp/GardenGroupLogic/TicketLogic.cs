ba49bfc57cf9c43ec810fb72d28d4cf612d9b850
Floortje Tjeertes <43434642+floriantjeertes@users.noreply.github.com>
2022-10-08 15:46:46 +0200

    commit

--- GardenGroupServicedeskApp/GardenGroupLogic/TicketLogic.cs
+++ GardenGroupServicedeskApp/GardenGroupLogic/TicketLogic.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GardenGroupModel;
 
-namespace GardenGroupModel
+namespace GardenGroupLogic
 {
     internal class TicketLogic
     {
+        //Client client = new Client();
         public void CreateTicket()
         {
-
         }
         public void UpdateTicker()
         {
