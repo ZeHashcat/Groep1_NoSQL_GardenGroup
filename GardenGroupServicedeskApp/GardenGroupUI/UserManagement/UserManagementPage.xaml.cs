33665889a7ab0a822e32505b6b73087af74587ed
LuukBakkum <luuk.bakkum2000@gmail.com>
2022-10-24 16:23:22 +0200

    update/24-10/16:23

---
+++ GardenGroupServicedeskApp/GardenGroupUI/UserManagement/UserManagementPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GardenGroupUI.UserManagement
+{
+    /// <summary>
+    /// Interaction logic for UserManagementPage.xaml
+    /// </summary>
+    public partial class UserManagementPage : Page
+    {
+        public UserManagementPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AppMenuWindow appWindow = new AppMenuWindow("../UserManagement/UserManagementPage.xaml");
+            appWindow.Show();
+        }
+    }
+}
